291383ea9a4162a544da42a6b2377fa6c65027a3
kim2christine <91278521+kim2christine@users.noreply.github.com>
2024-02-28 12:35:24 -0500

    Add files via upload

--- website.js
+++ website.js
@@ -7,3 +7,19 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+// Form Submission
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // You can add your email sending logic here
+  // For demonstration purposes, let's just log the form data
+  const formData = new FormData(this);
+  formData.forEach((value, key) => {
+    console.log(key, ":", value);
+  });
+
+  // Clear the form fields after submission
+  this.reset();
+});
